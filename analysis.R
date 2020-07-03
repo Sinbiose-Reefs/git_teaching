1530b41dd7dfccf16de415b211d5ab4869123ced
Diego Barneche <diego.barneche@ymail.com>
2020-07-03 20:00:43 +0800

    Dados agora disponiveis no ambiente global, com adicao de modelo linear

--- analysis.R
+++ analysis.R
@@ -1,3 +1,11 @@
+source("R/packages.R")
 source("R/functions.R")
-read_and_plot(filename = "data/data.csv",
-              plotname = "output/plot.pdf")
+
+data <- read.csv("data/data.csv", header = TRUE,
+                 stringsAsFactors = FALSE)
+
+data %>%
+  plot_my_data(plotname = "output/plot.pdf")
+
+model <- coef(summary(lm(y ~ x, data = data)))
+write.csv(model, "output/modelo.csv")
