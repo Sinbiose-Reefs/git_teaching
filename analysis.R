913bc58bb53146c57631dfe558d1f087dcc9d1bd
Diego Barneche <diego.barneche@ymail.com>
2020-07-03 19:10:22 +0800

    Salvei plot para pdf

--- analysis.R
+++ analysis.R
@@ -1,2 +1,7 @@
-data <- read.csv("data/data.csv", header = TRUE, stringsAsFactors = FALSE)
-plot(y ~ x, data = data)
+data <- read.csv("data/data.csv", header = TRUE,
+                 stringsAsFactors = FALSE)
+
+dir.create("output")
+pdf("output/plot.pdf", width = 7, height = 7)
+  plot(y ~ x, data = data)
+dev.off()
